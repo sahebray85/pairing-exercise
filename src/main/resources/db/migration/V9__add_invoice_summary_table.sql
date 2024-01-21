5d7357c8a5370f1a1f5f7e526a94a365aa34c1f4
SANKHA RAY <saheb_ray85@yahoo.co.in>
2024-01-21 14:18:22 +0100

    Schema for invoice

--- src/main/resources/db/migration/V9__add_invoice_summary_table.sql
+++ src/main/resources/db/migration/V9__add_invoice_summary_table.sql
@@ -1,12 +1,11 @@
-CREATE TABLE IF NOT EXISTS organisations_schema.organisations
+CREATE TABLE IF NOT EXISTS organisations_schema.invoice_summary
 (
     id                 UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-    name               VARCHAR     NOT NULL,
-    date_founded        DATE        NOT NULL,
-    country_code        CHAR(2)     NOT NULL,
-    VAT_number          VARCHAR(20),
-    registration_number VARCHAR(20),
-    local_address       VARCHAR(100),
-    legal_entity_type    VARCHAR(30) NOT NULL,
-    contact_details_id   VARCHAR(36) NOT NULL
-);
+    buyer_id           VARCHAR(50)   NOT NULL,
+    merchant_id        UUID   NOT NULL,
+    order_id           VARCHAR(50),
+    total_amount       FLOAT(10, 2) NOT NULL,
+    currency_code      VARCHAR(3) NOT NULL,
+    order_created      TIMESTAMP NOT NULL,
+    order_updates      TIMESTAMP NOT NULL
+ );
